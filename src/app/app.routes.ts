621d33486c70a6735c611dafd9a3ad24bc155795
mise-a-jour-backend <bobypro225@gmail.com>
2026-06-08 00:15:23 +0200

    pages activées

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
-
+import { About } from './features/about/about';
+import { Contact } from './features/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: About },
+  { path: 'apropos', component: About },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' }
 
 ];
